dea9b64ee66dbfc88af0e55236e617c8dd4e1721
harshabakku <sriharshabakku@gmail.com>
2022-10-25 18:56:23 +0530

    Ingest/Update asset metadata to elasticsearch periodically near real time (every 60 secs), compute es-indexName using NFT CollectionName (generic code)

--- routes/api/data.js
+++ routes/api/data.js
@@ -6,10 +6,10 @@ const client = require('../../elasticsearch/connection');
 require('events').EventEmitter.defaultMaxListeners = 0; //several async functions running in parallel while indexing assets to elasticsearch. //bulkIndex, batchProcessIndexing to elasticsearch is another solution. 
 
 const algoSeasListingsURL = `https://d3ohz23ah7.execute-api.us-west-2.amazonaws.com/prod/marketplace/v2/assetsByCollection/AlgoSeas%20Pirates?type=listing&sortBy=price&sortAscending=true&limit=500`;
-
+const intervalDuration =  3600000  //dataFetchandIndexInterval duration in milli secs
 
 //convert booleans to string as elasticsearch schema fails to understand  {"Shirts": "Flow"}, if {"Shirts": false} is already added for other document(asset)
- function replacer(key, value) {
+function replacer(key, value) {
     if (typeof value === "boolean") {
       return String(value);
     }
@@ -17,9 +17,8 @@ const algoSeasListingsURL = `https://d3ohz23ah7.execute-api.us-west-2.amazonaws.
   }
 
 router.get('/indexAllDocs', async function (req, res) {
-    console.log('Loading Application...')
-    // res.json('Application Running...')
     
+   
     //======= Check that Elasticsearch is up and running =======\\ //only works for Elastic Cloud cluster
     pingElasticsearch = async () => {
         await client.ping(
@@ -36,7 +35,7 @@ router.get('/indexAllDocs', async function (req, res) {
     // ====== Get Data From AlgoSeas API (or Web3 in the future) and then index onto Elasticsearch
     indexAllDocs = async () => {
         try {
-             
+            
             console.log('Getting Data From AlgoSeas Listings API')
 
             const result = await axios.post(`${algoSeasListingsURL}`,{},{
@@ -59,42 +58,59 @@ router.get('/indexAllDocs', async function (req, res) {
                 assetProps = algoSeasListing.assetInformation.nProps.properties,
 
                 //add additional key fields to be indexed to es.
-                
                 assetProps.price = algoSeasListing.assetInformation.listing.price,
                 assetProps.listedAlgoAmount = algoSeasListing.marketActivity.listedAlgoAmount, //price and listedAlgoAmount are same...
                 assetProps.nName =  algoSeasListing.assetInformation.nName,
-                assetProps.id   = assetProps.nName.split("#")[1],  
                 
-                console.log(assetProps),
+                //http://localhost:9200/index/id  returns the asset properties/data where index is the collectionName and id is asset/pirate Id
+                esIndex =  algoSeasListing.assetInformation.nName.split("#")[0].replace(/ /g,'').toLowerCase(), //es allows index names without spaces and lowercase only 
+                assetProps.id   =  algoSeasListing.assetInformation.nName.split("#")[1],  
+                
+                
                 //convert booleans to string as elasticsearch schema fails to understand  {"Shirts": "Flow"}, if {"Shirts": false} is already added for other document(asset)
                 assetProps = JSON.parse(JSON.stringify(assetProps, replacer)),
 
                 console.log(assetProps),
 
                 await client.index({ 
-                    index: 'algoseaspirates', //need to be generic for any collection...
+                    index: esIndex, //generic for any collection...
                     id:  assetProps.id,
                     body: assetProps
                 }), (err, resp, status) => {
                     console.log(resp);
+
                 }
                                 
                 ));
 
             console.log('All Listings Assets Have Been Indexed!');
             
-            res.json(algoSeasListings)
+            // res.json('All Listings Assets Have Been Indexed!');
             
+            console.log( '\n\n\n\n\n', '...........Preparing For The Next Data Check In ' + intervalDuration/1000 + ' secs........... ');
+            
+            // res.json(algoSeasListings)            
             
         } catch (err) {
             console.log(err)
         };
-
-        console.log('Preparing For The Next Data Check...');
+        
     }
-
+    
+    //fetch and index data for the first time. 
     pingElasticsearch()
     indexAllDocs()
+
+
+    //setInterval used to ingest data automatically in near real-time! //interval can be set to as low as 1min
+    // this ensures that gaming NFTs properties to be in sync with elasticsearch NFTs.
+            
+    // in the future future this fetchData and Index action should be incrementallly updating only the assets whose props have changed/updated since the last run 
+    setInterval(() => {
+        pingElasticsearch()
+        indexAllDocs()
+    }, intervalDuration); 
+
 });
  
 module.exports = router;
